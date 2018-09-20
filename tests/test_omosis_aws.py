b8b72f183e89b7e890838a773ad58f9f667e6990
MarcusJones <jones.0bj3@gmail.com>
2018-09-20 12:33:53 +0200

    update Duplicate test deleted

--- tests/test_omosis_aws.py
+++ tests/test_omosis_aws.py
@@ -1,16 +1,38 @@
 from osmosis_driver_interface.osmosis import Osmosis
 from osmosis_aws_driver.data_plugin import S3_Plugin
 from filecmp import cmp
-import os
+import os, sys
 import time
 from osmosis_aws_driver.config_parser import load_config_section
+import logging
+
+################################### SETUP LOGGING! ###################################
+loggers_dict = logging.Logger.manager.loggerDict
+
+logger = logging.getLogger()
+logger.handlers = []
+
+# Set level
+logger.setLevel(logging.DEBUG)
+
+# Create formatter
+FORMAT = "%(asctime)s - %(levelno)s - %(module)-15s - %(funcName)-15s - %(message)s"
+#FORMAT = "%(asctime)s L%(levelno)s: %(message)s"
+
+DATE_FMT = "%Y-%m-%d %H:%M:%S"
+formatter = logging.Formatter(FORMAT, DATE_FMT)
+
+# Create handler and assign
+handler = logging.StreamHandler(sys.stderr)
+handler.setFormatter(formatter)
+logger.handlers = [handler]
+logger.debug("Started logging in test module".format())
+################################### SETUP LOGGING! ###################################
+
+# Why was this needed???
 
 # os.environ['AWS_PROFILE'] = 'ocean'
-# Why was this needed???
 #aws = Osmosis('./tests/aws.ini').data_plugin()
-
-
-
 # def test_plugin_type():
 #     assert aws.type == 'AWS'
 
